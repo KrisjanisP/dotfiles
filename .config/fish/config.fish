c8f0621b03c70306c02fec54bce0efc59a34168c
KrisjanisP <krisjanispetrucena@gmail.com>
2023-07-21 15:26:48 +0300

    add fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,15 @@ end
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 alias ls="exa -lha --git --icons --color=always --group-directories-first"
+
+set -U fish_user_paths $fish_user_paths /home/krisjanisp/.local/share/gem/ruby/3.0.0/bin
+
+function catall
+    for file in (/usr/bin/ls)
+        if test -f $file
+            bat $file
+            echo "====== End of $file ======"
+        end
+    end
+end
+
